5bb87ab19f3cb5244af68c89a7057562acb6bb78
xgui4 <134389196+xgui4@users.noreply.github.com>
2026-01-13 19:31:22 -0500

    remove useless file

--- unsafe-detector.sh
+++ unsafe-detector.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-if [ $1 = "--help" ]; then 
+if [ "$1" = "--help" ]; then 
     echo "unsafe-detector.sh [directory to scan]"
+fi
 
 sudo find "$1" -type f -name '*.rs' -exec grep -H 'unsafe' {} + | wc -l
